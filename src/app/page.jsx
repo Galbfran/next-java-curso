d261c7ec8db16b997c88464655dc797e9bdf606d
Galbfran <francogalbiati984@gmail.com>
2023-10-14 10:48:58 -0300

    subo configuracion

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        hola mundo
+      </div>
+      <button className="btn btn-primary">hola</button>
+    </main>
+  )
+}
